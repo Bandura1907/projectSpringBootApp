af855799974c75148a0fe79aac0dcf092c594bfc
Dima Binkovskiy <dimmaa9@gmail.com>
2021-01-28 20:27:56 +0200

    Добавлено генератор ворд/файла

--- src/main/java/com/example/ThymeleafJpaProject/controllers/IndexController.java
+++ src/main/java/com/example/ThymeleafJpaProject/controllers/IndexController.java
@@ -2,14 +2,24 @@ package com.example.ThymeleafJpaProject.controllers;
 
 import com.example.ThymeleafJpaProject.models.RadioStation;
 import com.example.ThymeleafJpaProject.repositories.RadioStationRepo;
+
+import org.apache.commons.io.FileUtils;
+import org.docx4j.model.table.TblFactory;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.Tbl;
+import org.docx4j.wml.Tc;
+import org.docx4j.wml.Tr;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.List;
 
 
 @Controller
@@ -33,6 +43,39 @@ public class IndexController {
     public String createPage(Model model) {
         model.addAttribute("station", new RadioStation());
         return "createForm";
+    }
+
+    @RequestMapping(value="/generate", method=RequestMethod.GET)
+    public ResponseEntity<byte[]> getPDF() {
+        List<RadioStation> radioStations = repo.findAll();
+
+        File exportFile = new File("welcome.docx");
+        byte[] contents;
+        try {
+
+            WordprocessingMLPackage wordPackage = WordprocessingMLPackage.createPackage();
+            MainDocumentPart mainDocumentPart = wordPackage.getMainDocumentPart();
+
+            mainDocumentPart.addStyledParagraphOfText("Title", "Document №");
+            for(var item : radioStations){
+                mainDocumentPart.addParagraphOfText(item.getRadiostation_name() + " - " +
+                        item.getCount());
+            }
+
+            wordPackage.save(exportFile);
+
+            contents = FileUtils.readFileToByteArray(new File("C:\\Users\\Дмитрий\\Desktop\\projectSpringBootApp\\welcome.docx"));
+            HttpHeaders headers = new HttpHeaders();
+
+            // Here you have to set the actual filename of your pdf
+            String filename = "output.docx";
+            headers.setContentDispositionFormData(filename, filename);
+            headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+            ResponseEntity<byte[]> response = new ResponseEntity<>(contents, headers, HttpStatus.OK);
+            return response;
+        }catch (Exception e){
+            return new ResponseEntity("Err", HttpStatus.NOT_ACCEPTABLE);
+        }
     }
 
     @PostMapping("/save")
